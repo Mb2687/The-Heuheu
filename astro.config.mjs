74f55826a514c46f6f59e23d20f3139dfac7fc69
Mb2687 <michael.bonafede87@gmail.com>
2026-01-09 13:49:08 -0500

    Fix import syntax for tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://theheuheu.com',
-  output: 'static',
 });
